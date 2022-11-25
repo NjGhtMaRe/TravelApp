ab0443c7575885e20355c6124113210d4ceaa043
Andy <andy@MacBook-Pro.local>
2022-11-25 11:53:59 +0700

    Make Title component

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import styles from './styles';
 
-const Title = props => {
-  console.log('props :>> ', props);
-  return <Text style={styles.title}>{props.text}</Text>;
+const Title = ({text}) => {
+
+  return <Text style={styles.title}>{text}</Text>;
 };
 
 Title.defaultProps = {
   text: 'Default Title',
 };
 
-export default Title;
+export default React.memo(Title);
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import Title from '../../components/Title';
 
 const Home = () => {
+
   return (
     <SafeAreaView>
-      <Title text="Where do you want to go" />
+      <Title text="Where do" />
+      <Title text="you want to go?" />
     </SafeAreaView>
   );
 };
 
-export default Home;
+export default React.memo(Home);
