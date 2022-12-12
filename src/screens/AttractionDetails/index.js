b68e5527430b951e996a1820cc6b1c495560c75c
Andy <andy@MacBook-Pro.local>
2022-12-12 16:02:48 +0700

    Add mini image

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Text, SafeAreaView, ImageBackground } from 'react-native';
+import { Text, SafeAreaView, ImageBackground, Image, View } from 'react-native';
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 import styles from './styles';
 
 const AttractionDetails = ({ navigation, route }) => {
   const { item } = route?.params || {};
-  const mainImage = item?.images.length ? item.images[0] : null;
+  const mainImage = item?.images?.length ? item.images[0] : null;
+  const slicedImage = item?.images?.length ? item?.images.slice(0,5) : []
   const onBack = () => {
     navigation.goBack()
   }
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground style={styles.mainImage} imageStyle={{ borderRadius: 20 }} source={{ uri: mainImage }} />
-      <Text onPress={onBack}style={{ margin: 32}}>BACK</Text>
+      <ImageBackground 
+        style={styles.mainImage} 
+        imageStyle={{ borderRadius: 20 }} 
+        source={{ uri: mainImage }}
+      >
+        <View style={styles.header}>
+          <Pressable hitSlop={8}>
+            <Image onPress={onBack}style={styles.icon} source={require('../../assets/back.png')}/>
+          </Pressable>
+          <Pressable hitSlop={8}>
+            <Image onPress={onBack}style={styles.icon} source={require('../../assets/share.png')}/>
+          </Pressable>
+        </View>
+        <View style={styles.footer}>
+          {slicedImage?.map(image => (
+            <Image key={image} source={{ uri: image }} style={styles.miniImage} />
+          ))}
+        </View>
+      </ImageBackground> 
       <Text>{item?.name}</Text>
     </SafeAreaView>
   );
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
-
 const { height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -10,6 +10,36 @@ const styles = StyleSheet.create({
   mainImage: {
     width: '100%',
     height: height/2,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    margin: 16,
+  },
+  miniImage: {
+    width: 40,
+    height: 40,
+    margin: 8,
+    borderRadius: 10,
+    marginHorizontal: 4,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    backgroundColor: 'rgba(256,256,256,0.35)',
+    margin: 16,
+    paddingHorizontal: 8,
   }
 });
 
